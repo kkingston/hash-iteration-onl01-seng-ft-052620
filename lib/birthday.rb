8934a7fc66b2058a7b98aaa0b1ae2e19c1a15803
Kayla Kingston <kkingston09@gmail.com>
2020-06-03 20:50:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,7 +8,7 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  elsif age < 13 
+  elsif age > 13 
     puts "You are too old for this."
   end
 end
